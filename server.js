3ffc606622ad744659b2a7ef46452d0921090d60
mads2 <mads2k5@gmail.com>
2019-02-25 20:31:12 -0300

    Now with the server right

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = normalizePort(process.env.PORT || '3000');
+const port = process.env.PORT || '8080';
 app.set('port', port);
 
 const server = http.createServer(app);
