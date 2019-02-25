84c9e4683f68f9a9be102e4072fa3a6ddc2af523
mads2 <mads2k5@gmail.com>
2019-02-25 20:22:51 -0300

    Now with the server

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 'use strict'
 
-console.log('testando');
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+var route = router.get('/', (req, res, next) =>
+{
+    res.status(200).send({
+        title: "Node Storm API",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+server.listen(port);
+console.log('API rodando na porta ' + port);
+
